2160ba77f7e25cb0cf8a80984dbcf1b9503a9d76
Mukul Gupta <guptamukul06march@gmail.com>
2026-08-01 13:13:19 +0530

    initialize commit

--- python/first.py
+++
@@ -1,6 +0,0 @@
-# print("Hello World")
-x = int(input())
-y = int(input())
-
-print(x + y)
-
--- python/water_reminder.py
+++
@@ -1,5 +0,0 @@
-import os
-import time 
-
-command = "osascript -e \'say \"Hey Harry drink water\"\'; osascript -e \'display alert \"Hey Harry, Drink water\"\'"
-os.system(command)
